8dd58e366dc120c6d8a0ea00bf494fe61a7f69ec
bianis <febiana.anistya@gmail.com>
2019-04-30 17:56:12 +0700

    first

---
+++ src/tubes/pkg2/sbd/TUBES2SBD.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes.pkg2.sbd;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class TUBES2SBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int pilihan;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("[1] Tampilkan BFR & Fan-out Ratio");
+        System.out.println("[2] Total Blok Data + Index");
+        System.out.println("[3] Jumlah Box yang diakses untuk Pencarian Rekord");
+        System.out.println("[4] QEP dan Cost");
+        System.out.println("[5] File Shared Pool");
+        System.out.print("OPTION : ");
+        pilihan = input.nextInt();
+        
+        if (pilihan == 1){
+
+        } else if (pilihan == 2){
+
+        } else if (pilihan == 3){
+
+        } else if (pilihan == 4){
+
+        } else if (pilihan == 5){
+
+        }
+        
+    }
+    
+}
