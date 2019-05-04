47b4fb64d6cee390de6af46ef875132934b7bc5d
bianis <febiana.anistya@gmail.com>
2019-05-04 14:42:09 +0700

    help me yam binun di class BfrFr

--- src/process/BfrFr.java
+++ src/process/BfrFr.java
@@ -5,14 +5,73 @@
  */
 package process;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 /**
  *
- * @author user
+ * @author Febiana
  */
 public class BfrFr {
+
+    // Path punya alief
+    String dataDictionary = "src/data/DataDictionary.txt";
+    String kendaraan = "C:\\Users\\user\\Desktop\\SOURCE SBD\\TUBES-SBD-master/src/file/kendaraan.csv";
+    String transactions = "C:\\Users\\user\\Desktop\\SOURCE SBD\\TUBES-SBD-master/src/file/transactions.csv";
+    String users = "C:\\Users\\user\\Desktop\\SOURCE SBD\\TUBES-SBD-master/src/file/users.csv";
+
     
-    public void tesuto(){
-        System.out.println("yeay berhasil masuk");
-    };
+    String line = "";
+    String csvSplitBy = ";";
+    String[] tableKendaraan = new String[100];
+    String[] tableTransactions = new String[100];
+    String[] tableUsers = new String[100];
+    String[] tableInit = new String[100];
     
+    public void BFRandFR() {
+        //masukkan data pada csv kendaraan kedalam array tableKendaraan
+        try (BufferedReader br = new BufferedReader(new FileReader(dataDictionary))) {
+            while ((line = br.readLine()) != null) {
+                tableInit = line.split(csvSplitBy);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 8; i++) {
+            System.out.println(tableInit[i]);
+        }
+        
+    }
+   
+    public void showUsers(String name, int B, int P, int R, int v) {
+        double x = countBfr(B,R);
+        double y = countFanoutRatio(B,P,v);
+        System.out.println("BFR - "+name+" : "+x);
+        System.out.println("Fan Out Ratio - "+name+" : "+y);
+    }
+    
+    public void showTransactions(String name, int B, int P, int R, int v) {
+        double x = countBfr(B,R);
+        double y = countFanoutRatio(B,P,v);
+        System.out.println("BFR - "+name+" : "+x);
+        System.out.println("Fan Out Ratio - "+name+" : "+y);
+    }
+    
+    public void showKendaraan(String name, int B, int P, int R, int v) {
+        double x = countBfr(B,R);
+        double y = countFanoutRatio(B,P,v);
+        System.out.println("BFR - "+name+" : "+x);
+        System.out.println("Fan Out Ratio - "+name+" : "+y);
+    }
+    
+    public double countBfr(int B, int R){
+        int x = B/R;
+        double hasil = Math.floor(x);
+        return hasil;
+    }
+    
+    public double countFanoutRatio(int B, int P, int v) {
+        return Math.ceil(B/(v+P));
+    }
 }
--- src/process/TUBES2SBD.java
+++ src/process/TUBES2SBD.java
@@ -6,9 +6,25 @@
 package process;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,29 +35,13 @@ public class TUBES2SBD {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         int menu = 1;
-        
+
 //        Semua inisialisasi
-        String dictionary = "src/data/DataDictionary.txt";
         Scanner input = new Scanner(System.in);
         BfrFr op1 = new BfrFr();
-        String[] tableSemua = new String[100];
-        String line = "";
-        
-        try (BufferedReader br = new BufferedReader(new FileReader(dictionary))) {
-            while (((line = br.readLine()) != null) && ((line = br.readLine()) != "#")) {
-                System.out.println(line);
-                System.out.println("");
-                tableSemua = line.split(";");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        
-        
-        
+
         while ((menu >= 1) && (menu <= 5)) {
             System.out.println("");
             System.out.println(">> Main Menu");
@@ -51,20 +51,19 @@ public class TUBES2SBD {
             System.out.println("   4. Create QEP and calculate Query cost");
             System.out.println("   5. Print the content of Shared Pool file");
             System.out.print(">> Insert menu: ");
-            menu = input.nextInt();        
-            if (menu == 1){
-                op1.tesuto();
-            } else if (menu == 2){
+            menu = input.nextInt();
+            if (menu == 1) {
+                op1.BFRandFR();
+            } else if (menu == 2) {
 
-            } else if (menu == 3){
+            } else if (menu == 3) {
 
-            } else if (menu == 4){
+            } else if (menu == 4) {
 
-            } else if (menu == 5){
+            } else if (menu == 5) {
 
             }
         }
-        
-    }
-    
+}
+
 }
