b36067a9917158fec20f36d350da25fd5d6f1cf9
bianis <febiana.anistya@gmail.com>
2019-05-04 18:59:36 +0700

    dibereskan dan proses masukin dictonary pindah ke main

--- src/process/ProcessOneTwo.java
+++ src/process/ProcessOneTwo.java
@@ -18,113 +18,15 @@ import java.util.Scanner;
  */
 public class ProcessOneTwo {
 
-    // inisialisasi
-    String dataDictionary = "src/data/DataDictionary.txt";
-    String line = "";
-    String splitBy = ";";
-    String[] tabelPB, tabelPembeli, tabelPembelian, tabelBarang, tempTable;
-    List<String[]> allTableAL = new ArrayList<>();
-
-    public void BFRandFR() {
-//      read dataDictionary lalu masukan ke tabel semetara (allTable) yang akan di add ke arraylist seluruh table yang terbaca (allTableAL) *AL = ArrayList
-        try (BufferedReader br = new BufferedReader(new FileReader(dataDictionary))) {
-            while ((line = br.readLine()) != null) {
-                tempTable = line.split(splitBy);
-                allTableAL.add(tempTable);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-//      Mengkategorikan table dari allTableAL
-        tabelPB = allTableAL.get(0);
-        tabelPembeli = allTableAL.get(1);
-        tabelBarang = allTableAL.get(2);
-        tabelPembelian = allTableAL.get(3);
-
-//      Ambil nilai P dan B dari tabelPB  
-        int P = Integer.parseInt(tabelPB[0].split(" ")[1]);
-        int B = Integer.parseInt(tabelPB[1].split(" ")[1]);
-
-        System.out.println("");
-
-//      Pembeli
-        int Rp = Integer.parseInt(tabelPembeli[4].split(" ")[1]);
-        int np = Integer.parseInt(tabelPembeli[5].split(" ")[1]);
-        int Vp = Integer.parseInt(tabelPembeli[6].split(" ")[1]);
-        System.out.println("BFR " + tabelPembeli[0] + " : " + hitungBfr(B, Rp));
-        System.out.println("Fan Out Ratio " + tabelPembeli[0] + " : " + hitungFr(B, P, Vp));
-        System.out.println("");
-
-//      Barang
-        int Rb = Integer.parseInt(tabelBarang[4].split(" ")[1]);
-        int nb = Integer.parseInt(tabelBarang[5].split(" ")[1]);
-        int Vb = Integer.parseInt(tabelBarang[6].split(" ")[1]);
-        System.out.println("BFR " + tabelBarang[0] + " : " + hitungBfr(B, Rb));
-        System.out.println("Fan Out Ratio " + tabelBarang[0] + " : " + hitungFr(B, P, Vb));
-        System.out.println("");
-
-//      Pembelian
-        int Rpn = Integer.parseInt(tabelPembelian[6].split(" ")[1]);
-        int npn = Integer.parseInt(tabelPembelian[7].split(" ")[1]);
-        int Vpn = Integer.parseInt(tabelPembelian[8].split(" ")[1]);
-        System.out.println("BFR " + tabelPembelian[0] + " : " + hitungBfr(B, Rpn));
-        System.out.println("Fan Out Ratio " + tabelPembelian[0] + " : " + hitungFr(B, P, Vpn));
+    public void tampil1(int B, int P, int R, int V, String[] tabel) {
+        System.out.println("BFR " + tabel[0] + " : " + hitungBfr(B, R));;
+        System.out.println("Fan Out Ratio " + tabel[0] + " : " + hitungFr(B, P, V));
         System.out.println("");
     }
 
-    public void dataAndIndexBlocks() {
-        // inisialisasi
-        String dataDictionary = "src/data/DataDictionary.txt";
-        String line = "";
-        String splitBy = ";";
-        String[] tabelPB, tabelPembeli, tabelPembelian, tabelBarang, tempTable;
-        List<String[]> allTableAL = new ArrayList<>();
-
-//      read dataDictionary lalu masukan ke tabel semetara (allTable) yang akan di add ke arraylist seluruh table yang terbaca (allTableAL) *AL = ArrayList
-        try (BufferedReader br = new BufferedReader(new FileReader(dataDictionary))) {
-            while ((line = br.readLine()) != null) {
-                tempTable = line.split(splitBy);
-                allTableAL.add(tempTable);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-//      Mengkategorikan table dari allTableAL
-        tabelPB = allTableAL.get(0);
-        tabelPembeli = allTableAL.get(1);
-        tabelBarang = allTableAL.get(2);
-        tabelPembelian = allTableAL.get(3);
-
-//      Ambil nilai P dan B dari tabelPB  
-        int P = Integer.parseInt(tabelPB[0].split(" ")[1]);
-        int B = Integer.parseInt(tabelPB[1].split(" ")[1]);
-
-        System.out.println("");
-        
-//      Pembeli
-        int Rp = Integer.parseInt(tabelPembeli[4].split(" ")[1]);
-        int np = Integer.parseInt(tabelPembeli[5].split(" ")[1]);
-        int Vp = Integer.parseInt(tabelPembeli[6].split(" ")[1]);
-        System.out.println("Tabel Data " + tabelPembeli[0] + " : " + countBlockData(B, Rp, np));
-        System.out.println("Indeks " + tabelPembeli[0] + " : " + countBlockIndex(B, P, np, Vp));
-        System.out.println("");
-
-//      Barang
-        int Rb = Integer.parseInt(tabelBarang[4].split(" ")[1]);
-        int nb = Integer.parseInt(tabelBarang[5].split(" ")[1]);
-        int Vb = Integer.parseInt(tabelBarang[6].split(" ")[1]);
-        System.out.println("Tabel Data " + tabelBarang[0] + " : " + countBlockData(B, Rb, nb));
-        System.out.println("Indeks " + tabelBarang[0] + " : " + countBlockIndex(B, P, nb, Vb));
-        System.out.println("");
-
-//      Pembelian
-        int Rpn = Integer.parseInt(tabelPembelian[6].split(" ")[1]);
-        int npn = Integer.parseInt(tabelPembelian[7].split(" ")[1]);
-        int Vpn = Integer.parseInt(tabelPembelian[8].split(" ")[1]);
-        System.out.println("Tabel Data " + tabelPembelian[0] + " : " + countBlockData(B, Rpn, npn));
-        System.out.println("Indeks " + tabelPembelian[0] + " : " + countBlockIndex(B, P, npn, Vpn));
+    public void tampil2(int B, int P, int R, int V, int n, String[] tabel) {
+        System.out.println("Tabel Data " + tabel[0] + " : " + countBlockData(B, R, n));
+        System.out.println("Indeks " + tabel[0] + " : " + countBlockIndex(B, P, n, V));
         System.out.println("");
     }
 
--- src/process/TUBES2SBD.java
+++ src/process/TUBES2SBD.java
@@ -10,19 +10,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -35,15 +27,53 @@ public class TUBES2SBD {
     /**
      * @param args the command line arguments
      */
-
-
     public static void main(String[] args) throws FileNotFoundException {
 
-//      Semua inisialisasi
+        // Semua inisialisasi
         int menu = 1;
         Scanner input = new Scanner(System.in);
-        ProcessOneTwo op1 = new ProcessOneTwo();
+        ProcessOneTwo op12 = new ProcessOneTwo();
+        String dataDictionary = "src/data/DataDictionary.txt";
+        String line = "";
+        String splitBy = ";";
+        String[] tabelPB, tabelPembeli, tabelPembelian, tabelBarang, tempTable;
+        List<String[]> allTableAL = new ArrayList<>();
 
+        // read dataDictionary lalu masukan ke tabel semetara (allTable) yang akan di add ke arraylist seluruh table yang terbaca (allTableAL) *AL = ArrayList
+        // aku bikin di main biar bisa dipake menu 3 juga
+        try (BufferedReader br = new BufferedReader(new FileReader(dataDictionary))) {
+            while ((line = br.readLine()) != null) {
+                tempTable = line.split(splitBy);
+                allTableAL.add(tempTable);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Mengkategorikan table dari allTableAL
+        tabelPB = allTableAL.get(0);
+        tabelPembeli = allTableAL.get(1);
+        tabelBarang = allTableAL.get(2);
+        tabelPembelian = allTableAL.get(3);
+
+        // Ambil nilai P dan B dari tabelPB  
+        int P = Integer.parseInt(tabelPB[0].split(" ")[1]);
+        int B = Integer.parseInt(tabelPB[1].split(" ")[1]);
+        // Pembeli
+        int Rp = Integer.parseInt(tabelPembeli[4].split(" ")[1]);
+        int np = Integer.parseInt(tabelPembeli[5].split(" ")[1]);
+        int Vp = Integer.parseInt(tabelPembeli[6].split(" ")[1]);
+        // Barang
+        int Rb = Integer.parseInt(tabelBarang[4].split(" ")[1]);
+        int nb = Integer.parseInt(tabelBarang[5].split(" ")[1]);
+        int Vb = Integer.parseInt(tabelBarang[6].split(" ")[1]);
+        // Pembelian
+        int Rpn = Integer.parseInt(tabelPembelian[6].split(" ")[1]);
+        int npn = Integer.parseInt(tabelPembelian[7].split(" ")[1]);
+        int Vpn = Integer.parseInt(tabelPembelian[8].split(" ")[1]);
+
+        
+        // MAIN MENU
         while ((menu >= 1) && (menu <= 5)) {
             System.out.println("");
             System.out.println(">> Main Menu");
@@ -55,9 +85,13 @@ public class TUBES2SBD {
             System.out.print(">> Insert menu: ");
             menu = input.nextInt();
             if (menu == 1) {
-                op1.BFRandFR();
+                op12.tampil1(B, P, Rp, Vp, tabelPembeli);
+                op12.tampil1(B, P, Rb, Vb, tabelBarang);
+                op12.tampil1(B, P, Rpn, Vpn, tabelPembelian);
             } else if (menu == 2) {
-                op1.dataAndIndexBlocks();
+                op12.tampil2(B, P, Rp, Vp, np, tabelPembeli);
+                op12.tampil2(B, P, Rb, Vb, nb, tabelBarang);
+                op12.tampil2(B, P, Rpn, Vpn, npn, tabelPembelian);
             } else if (menu == 3) {
 
             } else if (menu == 4) {
